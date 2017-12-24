c588cd201e1a8c07a5a1244a9208a79d4ab7800d
Jeffrey Penkar <jspenkar@gmail.com>
2017-12-23 21:56:29 -0600

    Hour division fixed.

--- js/components/millimetro/hour.js
+++ js/components/millimetro/hour.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Hour(hours) {
   return (
-    <div className='hours' style={{transform:`rotate(-${360 * 0.58333}deg)`}}>
+    <div className='hours' style={{transform:`rotate(-${360* hours / 43200}deg)`}}>
       <div className='hour'>
         <div className='contents'>12</div>
       </div>
